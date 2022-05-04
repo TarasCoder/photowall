d74f1665a4f44262707a853e7b8bebf1efa03513
unknown <4kaskad@gmail.com>
2022-05-04 23:30:59 +0300

    adding saving inputs to useState object functionality

--- src/components/AddPhoto.jsx
+++ src/components/AddPhoto.jsx
@@ -1,19 +1,45 @@
 import React from "react";
 import s from "./AddPhoto.module.css";
+import { useState } from "react";
 
 function AddPhoto() {
+  const [data, setData] = useState({
+    link: "",
+    descr: "",
+  });
+
+  const handleInput = (ev) => {
+    const { value, name } = ev.target;
+    setData({ ...data, [name]: value });
+  };
+
   const addPost = (ev) => {
     ev.preventDefault();
     console.log("New Post");
+    console.log(data);
   };
 
   return (
     <div>
       <h1 className="headerText">Photowall</h1>
       <form className={s.formGroup}>
-        <input type="text" placeholder="Link" />
-        <input type="text" placeholder="Description" />
-        <button className={s.addPostBtn} onClick={addPost}>POST</button>
+        <input
+          type="text"
+          value={data.link}
+          name="link"
+          onChange={handleInput}
+          placeholder="Link"
+        />
+        <input
+          type="text"
+          value={data.descr}
+          name="descr"
+          onChange={handleInput}
+          placeholder="Description"
+        />
+        <button className={s.addPostBtn} onClick={addPost}>
+          POST
+        </button>
       </form>
     </div>
   );
