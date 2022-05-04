5c4d7f5d04a9ff90f1f333d5114c9c17fd3e334f
unknown <4kaskad@gmail.com>
2022-05-04 22:49:33 +0300

    styled addPhoto page

--- src/components/AddPhoto.jsx
+++ src/components/AddPhoto.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import s from "./AddPhoto.module.css";
 
 function AddPhoto() {
+  const addPost = (ev) => {
+    ev.preventDefault();
+    console.log("New Post");
+  };
+
   return (
     <div>
-      <form action="">
-        <h1 className="headerText">Photowall</h1>
-        <input type="text" />
-        <input type="text" />
-        <button>POST</button>
+      <h1 className="headerText">Photowall</h1>
+      <form className={s.formGroup}>
+        <input type="text" placeholder="Link" />
+        <input type="text" placeholder="Description" />
+        <button className={s.addPostBtn} onClick={addPost}>POST</button>
       </form>
     </div>
   );
